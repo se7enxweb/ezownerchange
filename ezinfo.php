d63d436e272a2af4b30bae3fa0aa8f9271e7f8ac
7x <info@se7enx.com>
2024-10-01 00:24:07 -0700

    Update ezinfo.php version bump for next release

--- ezinfo.php
+++ ezinfo.php
@@ -13,7 +13,7 @@ class ezownerchangeInfo
     public static function info()
     {
         return array( 'Name' => "<a href='https://github.com/se7enxweb/ezownerchange'>eZ Owner Change</a>",
-                      'Version' => "1.0.1",
+                      'Version' => "1.3.0",
                       'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>7x</a>",
                       'License' => "GNU General Public License v2.0 (or later)",
                       'info_url' => "https://github.com/se7enxweb/ezownerchange"
