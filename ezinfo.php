5a5346d3176b534520fc21ae0b16f3d7a170294e
7x <info@se7enx.com>
2024-10-01 07:21:15 +0000

    Added: Added LICENSE Documentation in Markdown Format. Added working composer json and ezinfo credits file

---
+++ ezinfo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File containing the ezownerchange copyright information file
+ *
+ * @copyright Copyright (C) 1999 - 2024 7x. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or later)
+ * @version //autogentag//
+ * @package ezownerchange
+ */
+
+class ezownerchangeInfo
+{
+    public static function info()
+    {
+        return array( 'Name' => "<a href='https://github.com/se7enxweb/ezownerchange'>eZ Owner Change</a>",
+                      'Version' => "1.0.1",
+                      'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>7x</a>",
+                      'License' => "GNU General Public License v2.0 (or later)",
+                      'info_url' => "https://github.com/se7enxweb/ezownerchange"
+                    );
+    }
+}
+
+?>
